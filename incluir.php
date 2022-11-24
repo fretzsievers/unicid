4e30dda6231d68507129773c9fde8c2472e9d65b
fretzsievers <66531771+fretzsievers@users.noreply.github.com>
2022-11-24 17:58:08 -0300

    Add files via upload

---
+++ incluir.php
@@ -0,0 +1,28 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title> CRUD - PHP com mysqli (POO) </title>
+<body>
+<h3> CRUD - PHP com mysqli (POO) - Inclusão</h3>
+<?php
+	include_once('conexao.php');
+	//criando o objeto mysql e conectando ao banco de dados
+	$mysql = new BancodeDados();
+	$mysql->conecta();
+			
+	// recuperando 
+	$codigo = $_POST['codigo'];
+	$produto = $_POST['produto'];	
+	$descricao = $_POST['descricao'];	
+	$data = $_POST['data'];	
+	$valor = $_POST['valor'];	
+
+	// criando a linha de INSERT
+	$sqlinsert =  "insert into tabelaimg (codigo, produto, descricao, data, valor) values ($codigo, '$produto', '$descricao', '$data', $valor)";
+	
+	// executando instrução SQL através do método sqlstring()
+	$resultado = $mysql->sqlstring($sqlinsert,"INCLUSÃO");
+?>
+<br><br>
+<input type='button' onclick="window.location='index.php';" value="Voltar">
+</body>
+</html>
